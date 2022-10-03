b93a522c0eb8a6f0ae9816f7aa905a74549c256f
Ilya <ilyamonstr777@gmail.com>
2022-10-03 20:25:58 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        StringBuilder stringBuilder = new StringBuilder("StringBuilder");
+        stringBuilder.append(" - класс для часто изменяемых строк.");
+        System.out.println(stringBuilder);
+    }
+}
