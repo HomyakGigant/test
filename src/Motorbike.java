b12448f8387650c5fec8c921ae77619c7aee1113
Ilya <ilyamonstr777@gmail.com>
2024-08-20 20:54:09 +0500

    Second commit

--- src/Motorbike.java
+++ src/Motorbike.java
@@ -1,10 +1,12 @@
 public class Motorbike {
     private int year;
+    private int cost;
     private String model;
     private String color;
 
-    public Motorbike(int year, String model, String color) {
+    public Motorbike(int year,int cost, String model, String color) {
         this.year = year;
+        this.cost = cost;
         this.model = model;
         this.color = color;
     }
@@ -20,11 +22,14 @@ public class Motorbike {
     public String getColor() {
         return color;
     }
+    public int getCost() {
+        return cost;
+    }
     public static void main(String[] args) {
 
-        Motorbike motorbike = new Motorbike(2020, "Honda", "Black");
+        Motorbike motorbike = new Motorbike(2020,900000, "Honda", "Black");
         System.out.println(motorbike.getModel() + "\n" +
                 motorbike.getYear() + "\n" +
-                motorbike.getColor());
+                motorbike.getColor() + "\n" + motorbike.getCost());
     }
 }
