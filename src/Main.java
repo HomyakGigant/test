7cfe03f6c4cd52e6d011073b9f0e85f42e355048
Ilya <ilyamonstr777@gmail.com>
2024-08-21 18:43:01 +0500

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
 
     public static void main(String[] args) {
-        String str = "Рекс Лорд Лом Шарик Бобик Шалун";
-        Pattern pattern = Pattern.compile("Л[а-я]+");
-        Matcher matcher = pattern.matcher(str);
-        while (matcher.find()) {
-            System.out.println(matcher.group());
-        }
+        System.out.println("branch");
     }
 }
